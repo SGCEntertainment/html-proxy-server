7d5da3bad1cab2c55e0724953042e575c3bd7e9a
ZloyShcolnik <zloalways7@gmail.com>
2025-04-24 02:57:08 +0300

    init

--- server.js
+++ server.js
@@ -6,25 +6,12 @@ app.get("/proxy", async (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).send("No URL");
 
-  // Конфигурация прокси (без аутентификации)
-  const proxyConfig = {
-    host: '58.69.194.136',   // Замените на ваш внешний прокси сервер
-    port: 8081                   // Порт прокси сервера
-  };
-
   try {
-    // Запрос через прокси
     const response = await axios.get(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-      },
-      proxy: proxyConfig // Используем прокси-конфигурацию без аутентификации
+        'User-Agent': 'Mozilla/5.0'
+      }
     });
-    
     res.set("Content-Type", "text/html");
     res.send(response.data);
   } catch (err) {
