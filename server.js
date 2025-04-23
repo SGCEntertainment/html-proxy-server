41ef99b9dd1f5843d703e96094f1536a4b97f82c
ZloyShcolnik <zloalways7@gmail.com>
2025-04-24 02:37:03 +0300

    init

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.get("/proxy", async (req, res) => {
   try {
     const response = await axios.get(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
       }
     });
     res.set("Content-Type", "text/html");
